3318bcca09ddf3ba0a7e20a3f27a675c6ad3282e
Teemo0708 <jms3800@gmail.com>
2022-08-10 20:09:09 +0900

    2022.08.10커밋

---
+++ Ex04_변수4.js
@@ -0,0 +1,5 @@
+let age; //  값을 할당하지 않음 
+let user = null; // user 값이 null (user는 존재하지 않는다는 뜻)
+
+console.log(age) // 값을 할당하지 않았기 때문에 undefined 반환
+console.log(user) // null 반환
---
+++ Ex05_typeof연산자.js
@@ -0,0 +1,9 @@
+ // typeof : 변수의 자료형을 알아낼 수 있다
+ const name = "Mike";
+
+ console.log(typeof 3);
+ console.log(typeof name);
+ console.log(typeof true);
+ console.log(typeof "QWER");
+ console.log(typeof null);
+ console.log(typeof undefined);
