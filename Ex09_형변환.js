2ff18e212686007ebeeefb980efe4bba921a9f0e
Teemo0708 <jms3800@gmail.com>
2022-08-11 20:31:54 +0900

    2022.08.11커밋

---
+++ Ex09_형변환.js
@@ -0,0 +1,18 @@
+
+       
+    //    String() -> 문자형으로 변환
+    //    Number() -> 숫자형으로 변환
+    //    Boolean() -> 불리언형으로 변환
+        
+
+        console.log(
+            String(3),
+            String(true),
+            String(false),
+            String(null),
+            String(undefined),
+            Number("1234"),
+            Number(true),
+            Number(false),
+        );
+
