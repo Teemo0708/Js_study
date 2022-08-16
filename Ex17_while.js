89e12a85f72b2b7cb5e3c44e70e98116c607786b
양명호 <jms3800@gmail.com>
2022-08-16 20:13:25 +0900

    2022.0816 커밋

---
+++ Ex17_while.js
@@ -0,0 +1,19 @@
+
+       let num = 0;
+
+       while(num <= 10){ // while(true) 경우에 실행
+        console.log(`${num} 번째 반복하는 중`)
+        num++;
+       }
+
+    /*
+       do.. while의 경우
+
+        do{
+            // 코드
+            i++;
+        }while(i<10)
+        의 형태로 쓰여 조건문을 뒤로 뺼 수 있다. 이 경우 코드는 무조건 한번은 실행된다
+
+       */
+    
